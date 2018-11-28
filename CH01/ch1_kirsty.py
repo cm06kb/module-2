d994a10eec665ed6b110f987b97e9ec15a696035
kirsty bradley <cm06kb@gmail.com>
2018-11-28 17:08:11 +0100

    first module 2 assessment

---
+++ CH01/ch1_kirsty.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 28 12:55:42 2018
+
+@author: Gebruiker
+"""
+
+print(2 + 3)
+print(2.9 + 3.2)
+print((3.2/2.0) + 7)
+print(2**3)
+print(4**(1/2))
+print(str(6) + 'a')
+
+a = 2 + 3
+b = 2.9 + 3.2
+c = (3.2/2.0) + 7
+d = (2**3)
+e = 4**(1/2)
+f = str(6) + 'a'
+# strings are scalar, they cannot be subdivided (notice g and h both have a size of 1).
+g="cat ate lemons"
+h="cat"
+
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print(f)
+print(g)
+print(h)
---
+++ CH02/CH2_kirsty.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 27 17:18:12 2018
+
+@author: Gebruiker
+"""
+"""task 1"""
+print(5 - 6)
+print(8 * 9)
+print(6 / 2)
+print(5 / 2)
+print( 5.0 / 2)
+print(5 % 2)
+print(2 * (10 + 3))
+print(2 ** 4)
+ 
+"""Task 2 variable practise."""
+age = 5
+age = "almost three"
+a_longer_name = "hello, CFG!"
+print (age)
+print (a_longer_name)
